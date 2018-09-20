01663885c2c7c50ddb80ac58f711d8c684f4ad3c
scott <scott.a.magnuson.19@dartmouth.edu>
2018-09-20 15:23:36 -0400

    update

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-const App = () => {
-  return <div className="test">All the React are belong to us!</div>;
-};
-
-ReactDOM.render(<App />, document.getElementById('main'));
